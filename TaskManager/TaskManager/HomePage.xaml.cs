da58f31900cb49cbf0185bfe7feaf4fd064fde7f
jaspreet <ghotra.jaspreet003@gmail.com>
2024-03-04 16:03:28 -0500

    saving data

--- TaskManager/TaskManager/HomePage.xaml.cs
+++ TaskManager/TaskManager/HomePage.xaml.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
+using TaskManager.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,14 +12,62 @@ namespace TaskManager
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomePage : ContentPage
     {
+
+        string priority = string.Empty;
+
         public HomePage()
         {
             InitializeComponent();
+            
         }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
+            Task task = new Task();
 
+            
+
+            using (SQLiteConnection con = new SQLiteConnection(App.DatabaseLocation))
+            {
+                if (taskEntry.Text != null && selectedPriority.Text != null)
+                {
+                    priority = selectedPriority.Text.ToLower();
+
+                    if (priority != "high" || priority != "medium" || priority != "low")
+                    {
+                        DisplayAlert("Priority does not match", "Priority should be high, medium or low only", "OK");
+                    }
+                    else
+                    {
+
+                        task.Description = taskEntry.Text;
+
+                        task.Priority = selectedPriority.Text.ToLower();
+
+
+                        con.CreateTable<Task>();
+
+                        int row = con.Insert(task);
+
+                        con.Close();
+
+                        if (row > 0)
+                        {
+                            DisplayAlert("Success", "Task Inserted", "OK");
+                            taskEntry.Text = string.Empty;
+                        }
+                        else
+                        {
+                            DisplayAlert("Failed", "Cannot Save Task ", "OK");
+                        }
+
+                    }
+                }
+                else
+                {
+                    DisplayAlert("Empty Field", "Descritpion or priority caanot be empty", "OK");
+                }
+                }
         }
     }
 }
--- TaskManager/TaskManager/Model/Task.cs
+++ TaskManager/TaskManager/Model/Task.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace TaskManager.Model
 {
-    internal class Task
+    public class Task
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(300)]
+        public string Description { get; set; }   
+        
+        public string Priority { get; set; }
+
     }
 }
